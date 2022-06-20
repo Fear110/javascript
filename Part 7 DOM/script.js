2d2d5f01900a671c0b52c6e089c69d5bb1db5b18
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-06-20 17:21:00 +0545

    DOM first commit

---
+++ Part 7 DOM/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 100;
+
+document.querySelector('.score').textContent = 10;
+
+ 
