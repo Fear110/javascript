7dc7a27120b3ae7f21bd47227f825ad6e5783ebb
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-06-23 19:02:18 +0545

    Show Model Commit

--- Part 7 DOM/Window/script.js
+++ Part 7 DOM/Window/script.js
@@ -1,1 +1,31 @@
-'use strict'
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('.show-modal');
+
+btnOpenModal.addEventListener('click', function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+});
+
+/*
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+*/
+
+const closemodal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closemodal);
+overlay.addEventListener('click', closemodal);
