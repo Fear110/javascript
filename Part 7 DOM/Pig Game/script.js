294e27845d387346c81a1df6451fbe98c6f42e80
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-06-24 20:12:58 +0545

    Pig game Second Commit

--- Part 7 DOM/Pig Game/script.js
+++ Part 7 DOM/Pig Game/script.js
@@ -1,1 +1,50 @@
 'use strict';
+
+// document.querySelector('.btn--roll').addEventListener('click', function () {
+//   console.log('this is ');
+// });
+
+const rollDice = document.querySelector('.btn--roll');
+let totalNumber = 0;
+let playerOneTotal = 0;
+
+
+let firstRoll = function(){ rollDice.addEventListener('click', function () {
+  let randomNum = Math.trunc(Math.random() * 6 + 1);
+  console.log(randomNum);
+
+  if (randomNum === 6) {
+    totalNumber = totalNumber + randomNum;
+    document.querySelector('.current-score').textContent = totalNumber;
+    document.querySelector('.dice').src = 'dice-6.png';
+    playerOneTotal = totalNumber;
+  } else if (randomNum === 2) {
+    totalNumber = totalNumber + randomNum;
+    document.querySelector('.current-score').textContent = totalNumber;
+    document.querySelector('.dice').src = 'dice-2.png';
+    playerOneTotal = totalNumber;
+  } else if (randomNum === 3) {
+    totalNumber = totalNumber + randomNum;
+    document.querySelector('.current-score').textContent = totalNumber;
+    document.querySelector('.dice').src = 'dice-3.png';
+    playerOneTotal = totalNumber;
+  } else if (randomNum === 4) {
+    totalNumber = totalNumber + randomNum;
+    document.querySelector('.current-score').textContent = totalNumber;
+    document.querySelector('.dice').src = 'dice-4.png';
+    playerOneTotal = totalNumber;
+  } else if (randomNum === 5) {
+    totalNumber = totalNumber + randomNum;
+    document.querySelector('.current-score').textContent = totalNumber;
+    document.querySelector('.dice').src = 'dice-5.png';
+    playerOneTotal = totalNumber;
+  } else {
+    playerOneTotal = totalNumber;  
+    document.querySelector('.current-score').textContent = "0"; 
+    document.querySelector('.score').textContent = playerOneTotal;
+    document.querySelector('.dice').src = 'dice-1.png';
+  }
+});
+}
+
+firstRoll();
