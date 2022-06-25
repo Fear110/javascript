2ca43c73724a668a0866be90a0dc82f5450d79fe
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-06-25 09:34:35 +0545

    pig game second commit

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -4,6 +4,7 @@
 //   console.log('this is ');
 // });
 
+/*
 const rollDice = document.querySelector('.btn--roll');
 let totalNumber = 0;
 let playerOneTotal = 0;
@@ -16,28 +17,26 @@ let firstRoll = function(){ rollDice.addEventListener('click', function () {
   if (randomNum === 6) {
     totalNumber = totalNumber + randomNum;
     document.querySelector('.current-score').textContent = totalNumber;
-    document.querySelector('.dice').src = 'dice-6.png';
     playerOneTotal = totalNumber;
+    document.querySelector('.dice').src = 'dice-6.png';
   } else if (randomNum === 2) {
     totalNumber = totalNumber + randomNum;
     document.querySelector('.current-score').textContent = totalNumber;
-    document.querySelector('.dice').src = 'dice-2.png';
     playerOneTotal = totalNumber;
+    document.querySelector('.dice').src = 'dice-2.png';
   } else if (randomNum === 3) {
     totalNumber = totalNumber + randomNum;
     document.querySelector('.current-score').textContent = totalNumber;
-    document.querySelector('.dice').src = 'dice-3.png';
     playerOneTotal = totalNumber;
+    document.querySelector('.dice').src = 'dice-3.png';
   } else if (randomNum === 4) {
     totalNumber = totalNumber + randomNum;
     document.querySelector('.current-score').textContent = totalNumber;
+    playerOneTotal = totalNumber;
     document.querySelector('.dice').src = 'dice-4.png';
-    playerOneTotal = totalNumber;
   } else if (randomNum === 5) {
-    totalNumber = totalNumber + randomNum;
-    document.querySelector('.current-score').textContent = totalNumber;
+    
     document.querySelector('.dice').src = 'dice-5.png';
-    playerOneTotal = totalNumber;
   } else {
     playerOneTotal = totalNumber;  
     document.querySelector('.current-score').textContent = "0"; 
@@ -48,3 +47,13 @@ let firstRoll = function(){ rollDice.addEventListener('click', function () {
 }
 
 firstRoll();
+
+*/
+
+const scoreFirstElement = document.querySelector('#score--0');
+const scoreSecondElement = document.getElementById('score--1');
+const dicePicElement = document.querySelector('.dice');
+
+scoreFirstElement.textContent = 0;
+scoreSecondElement.textContent = 0;
+dicePicElement.classList.add('hidden');
