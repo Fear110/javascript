701590d72787ac8fd4ac6b9aba84a0e3708c4b5f
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-06-26 12:47:28 +0545

    pig game final commit

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -49,7 +49,8 @@ let firstRoll = function(){ rollDice.addEventListener('click', function () {
 firstRoll();
 
 */
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const scoreFirstElement = document.querySelector('#score--0');
 const scoreSecondElement = document.getElementById('score--1');
 const dicePicElement = document.querySelector('.dice');
@@ -57,24 +58,55 @@ const ButtonNew = document.querySelector('.btn--new');
 const ButtonRoll = document.querySelector('.btn--roll');
 const ButtonHold = document.querySelector('.btn--hold');
 const currentFirstScore = document.querySelector('#current--0');
-const currentSecondScore = document.querySelector('#current--1')
-
+const currentSecondScore = document.querySelector('#current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 scoreFirstElement.textContent = 0;
 scoreSecondElement.textContent = 0;
 dicePicElement.classList.add('hidden');
-
-let totalScore = 0;
 let currentScore = 0;
-
+let activePLayer = 0;
+const scores = [0, 0];
 ButtonRoll.addEventListener('click', function () {
   const randomNumber = Math.trunc(Math.random() * 6 + 1);
   dicePicElement.classList.remove('hidden');
   dicePicElement.src = `pic/dice-${randomNumber}.png`;
-
   if (randomNumber !== 1) {
     currentScore = currentScore + randomNumber;
-    currentFirstScore.textContent = currentScore;
+    document.getElementById(`current--${activePLayer}`).textContent =
+      currentScore;
   } else {
-    //if random number is 1
+    currentScore = 0;
+    document.getElementById(`current--${activePLayer}`).textContent = 0;
+    activePLayer = activePLayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
+});
+ButtonHold.addEventListener('click', function () {
+  scores[activePLayer] += currentScore;
+  document.getElementById(`score--${activePLayer}`).textContent =
+    scores[activePLayer];
+  currentScore = 0;
+  document.getElementById(`current--${activePLayer}`).textContent = 0;
+  activePLayer = activePLayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  if (scores[0] >= 10) {
+    document.querySelector(`.player--0`).classList.add('player--winner');
+    document.querySelector(`.player--0`).classList.remove('player--active');
+  } else if (scores[1] >= 10) {
+    document.querySelector(`.player--1`).classList.add('player--winner');
+    document.querySelector(`.player--1`).classList.remove('player--active');
+  }
+});
+
+ButtonNew.addEventListener('click', function () {
+  scoreFirstElement.textContent = 0;
+  scoreSecondElement.textContent = 0;
+  dicePicElement.classList.add('hidden');
+  document.querySelector(`.player--1`).classList.remove('player--winner');
+  document.querySelector(`.player--0`).classList.remove('player--winner');
+  scores[0] = 0;
+  scores[1] = 0;
 });
