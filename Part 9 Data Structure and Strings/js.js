fed292dfe4f1960b80ee8225525cb18927133833
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-07-08 13:12:52 +0545

    Part 09 Working with String last Commit

--- Part 9 Data Structure and Strings/js.js
+++ Part 9 Data Structure and Strings/js.js
@@ -784,7 +784,7 @@ checkBaggage('Got some snacks and a gun for protection');
 // const [firstname, lastname] = fullname.split(' ');
 // const fname = ['mero naam',firstname,lastname].join(' ');
 // console.log(fname);
-
+/*
 const fullname = function (names) {
   const splitnames = names.split(' ');
   let ffname = [];
@@ -807,6 +807,7 @@ const air = function (num) {
 
 air(9);
 air(90);
+*/
 
 /*
 Coding Challenge #4
@@ -853,4 +854,26 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
+*/
+// String Methods Practice
+
+/*
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(output);
+}
 */
